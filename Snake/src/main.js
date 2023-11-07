02b95f460321e481755137ff879edbdf7f389367
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2023-11-07 16:30:18 +0100

    07.11.2023
    Début du projet, création du rapport, création des différents fichiers

---
+++ Snake/src/main.js
@@ -0,0 +1,18 @@
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const move = () => {
+
+  // Dessine la grille de jeu
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(0, 0, 800, 800);
+
+  // Rafraichit à chaque seconde
+  setTimeout(() => {
+    requestAnimationFrame(move);
+  }, 1000);
+  
+};
+
+requestAnimationFrame(move);
