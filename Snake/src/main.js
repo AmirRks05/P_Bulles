30481b279e5224e4f36d5b0aaf999d91a8ec7f23
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2023-11-14 16:29:33 +0100

    14.11.2023
    Ajout du snake, possibilté de bouger

--- Snake/src/main.js
+++ Snake/src/main.js
@@ -1,18 +1,63 @@
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const TailleCarre = 40;
+
+const snake = [
+  { x: 280, y: 360 }
+];
+
+let dx = TailleCarre;
+let dy = 0;
+
 const move = () => {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
 
-  // Dessine la grille de jeu
-  ctx.fillStyle = 'grey';
-  ctx.fillRect(0, 0, 800, 800);
+  if (snake.length > 3) {
+    snake.pop();
+  }
 
-  // Rafraichit à chaque seconde
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = 'rgb(15, 199, 39)';
+  snake.forEach((segment) => {
+    ctx.fillRect(segment.x, segment.y, TailleCarre, TailleCarre);
+  });
+
   setTimeout(() => {
     requestAnimationFrame(move);
-  }, 1000);
-  
+  }, 80);
+
 };
 
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+    case 'w':
+    case '8':
+      dx = 0;
+      dy = -TailleCarre;
+      break;
+    case 'ArrowDown':
+    case 's':
+    case '2':
+      dx = 0;
+      dy = TailleCarre;
+      break;
+    case 'ArrowLeft':
+    case 'a':
+    case '4':
+      dx = -TailleCarre;
+      dy = 0;
+      break;
+    case 'ArrowRight':
+    case 'd':
+    case '6':
+      dx = TailleCarre;
+      dy = 0;
+      break;
+  }
+});
 requestAnimationFrame(move);
