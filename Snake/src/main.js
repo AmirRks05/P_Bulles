c3c61bba1ce471f3cfd49442fd4e4d41e675f500
Amir Zeqiri <amizeqiri@etmlnet.local>
2023-11-21 16:27:05 +0100

    21.11.2023
    Ajout de l'option des bordures, alerte lorsque le serpent touche les bordures, ajout brut du score, début du serpent à 0 et non 3

--- Snake/src/main.js
+++ Snake/src/main.js
@@ -1,63 +1,68 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
-const TailleCarre = 40;
-
-const snake = [
-  { x: 280, y: 360 }
-];
-
-let dx = TailleCarre;
+const BlockSize = 40;
+const snake = [{ x: 280, y: 360 }];
+let value = 0;
+let dx = BlockSize;
 let dy = 0;
 
 const move = () => {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+
+  // Boucle qui vérifie si le serpent touche les bords et affiche l'alerte avec le score
+  if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) {
+    alert("Perdu, votre score est " + value);
+    location.reload();
+  }
+
   snake.unshift(head);
 
-  if (snake.length > 3) {
+  if (snake.length > value) {
     snake.pop();
   }
 
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
   ctx.fillStyle = 'rgb(15, 199, 39)';
   snake.forEach((segment) => {
-    ctx.fillRect(segment.x, segment.y, TailleCarre, TailleCarre);
+    ctx.fillRect(segment.x, segment.y, BlockSize, BlockSize);
   });
+
+  ctx.font = '15px arial'
+  ctx.fillStyle = 'white';
+  ctx.fillText('Score : ' + value, 20, 30)
 
   setTimeout(() => {
     requestAnimationFrame(move);
   }, 80);
-
 };
 
+// Permettre de déplacer le serpent avec les différentes touches possibles
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
     case 'w':
-    case '8':
       dx = 0;
-      dy = -TailleCarre;
+      dy = -BlockSize;
       break;
     case 'ArrowDown':
     case 's':
-    case '2':
       dx = 0;
-      dy = TailleCarre;
+      dy = BlockSize;
       break;
     case 'ArrowLeft':
     case 'a':
-    case '4':
-      dx = -TailleCarre;
+      dx = -BlockSize; 
       dy = 0;
       break;
     case 'ArrowRight':
     case 'd':
-    case '6':
-      dx = TailleCarre;
+      dx = BlockSize;
       dy = 0;
       break;
   }
 });
+
 requestAnimationFrame(move);
