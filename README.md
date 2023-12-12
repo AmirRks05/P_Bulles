9dacddb88043356115208f011fdd5fef2328d35f
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2023-12-12 16:28:43 +0100

    12.12.2023
    Ajout de la fonction qui fait en sorte que le snake ne puisse pas revenir en arrière, avancement dans le support de cours, création du fichier readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# P_Bulles
+EN : Make a replica of the famous Snake game in JavaScript.\
+FR : Faire un réplica du célèbre jeu Snake en JavaScript.
--- Snake/src/main.js
+++ Snake/src/main.js
@@ -10,27 +10,36 @@ const apple = new Apple(canvas, ctx, BlockSize);
 const snake = new Snake(canvas, ctx, BlockSize, score, apple);
 
 // Permettre de déplacer le serpent avec les différentes touches possibles
+// Les boucles if font en sorte que le serpent ne puisse pas revenir en arrière
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
     case 'w':
-      snake.dx = 0;
-      snake.dy = -BlockSize;
+      if (snake.dy !== BlockSize) {
+        snake.dx = 0;
+        snake.dy = -BlockSize;
+      }
       break;
     case 'ArrowDown':
     case 's':
-      snake.dx = 0;
-      snake.dy = BlockSize;
+      if (snake.dy !== -BlockSize) {
+        snake.dx = 0;
+        snake.dy = BlockSize;
+      }
       break;
     case 'ArrowLeft':
     case 'a':
-      snake.dx = -BlockSize;
-      snake.dy = 0;
+      if (snake.dx !== BlockSize) {
+        snake.dx = -BlockSize;
+        snake.dy = 0;
+      }
       break;
     case 'ArrowRight':
     case 'd':
-      snake.dx = BlockSize;
-      snake.dy = 0;
+      if (snake.dx !== -BlockSize) {
+        snake.dx = BlockSize;
+        snake.dy = 0;
+      }
       break;
   }
 });
