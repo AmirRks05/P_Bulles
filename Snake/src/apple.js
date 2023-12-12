c39b41372d05eefe0e45a612b6fd9b21728b50c7
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2023-12-12 10:31:29 +0100

    12.12.2023 (P_Prod)
    Changement de couleur du fond, augmentation des bordures du canvas, avancement dans le support de cours

--- Snake/src/apple.js
+++ Snake/src/apple.js
@@ -1,18 +1,18 @@
 export class Apple {
-    constructor(canvas, ctx, BlockSize) {
-      this.canvas = canvas;
-      this.ctx = ctx;
-      this.BlockSize = BlockSize;
-      this.generate();
-    }
-  
-    generate() {
-      this.x = Math.floor(Math.random() * (this.canvas.width / this.BlockSize)) * this.BlockSize;
-      this.y = Math.floor(Math.random() * (this.canvas.height / this.BlockSize)) * this.BlockSize;
-    }
-  
-    draw() {
-      this.ctx.fillStyle = 'red';
-      this.ctx.fillRect(this.x, this.y, this.BlockSize, this.BlockSize);
-    }
+  constructor(canvas, ctx, BlockSize) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.BlockSize = BlockSize;
+    this.generate();
   }
+
+  generate() {
+    this.x = Math.floor(Math.random() * (this.canvas.width / this.BlockSize)) * this.BlockSize;
+    this.y = Math.floor(Math.random() * (this.canvas.height / this.BlockSize)) * this.BlockSize;
+  }
+
+  draw() {
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(this.x, this.y, this.BlockSize, this.BlockSize);
+  }
+}
